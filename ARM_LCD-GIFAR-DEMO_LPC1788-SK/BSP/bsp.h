de8add639c5d353891f7fedcb79b98ad7b679c90
EdizonTN <edizontn@gmail.com>
2014-08-08 14:43:53 +0200

    init

---
+++ ARM_LCD-GIFAR-DEMO_LPC1788-SK/BSP/bsp.h
@@ -0,0 +1,14 @@
+/***********************************************************
+ * bsp.h
+ *
+ *  Created on: 29.7.2014
+ *      Author: peterj
+ ***********************************************************/
+
+
+#ifndef BSP_H_
+#define BSP_H_
+
+	#include "phylan_ks8721b.h"  				// driver pre externy ethernet PHY
+
+#endif /* BSP_H_ */
---
+++ ARM_LCD-GIFAR-DEMO_LPC1788-SK/Include/global.h
@@ -0,0 +1,61 @@
+/***********************************************************
+ * global.h
+ *
+ *  Created on: 25.7.2014
+ *      Author: peterj
+ ***********************************************************/
+
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+// ************ SYSTEM HEADERS ****************************************************
+#include "stdint.h"
+
+
+// ************ APPLICATION CONFIG ************************************************
+#include "Configuration.h"
+
+
+
+
+
+// ************ CHIP HEADERS ******************************************************
+#include "lpc177x_8x_libcfg_default.h"
+//#include "../Chip/Include/arm_common_tables.h"
+//#include "../Chip/Include/arm_const_structs.h"
+//#include "../Chip/Include/arm_math.h"
+//#include "../Chip/Include/core_cm0.h"
+//#include "../Chip/Include/core_cm0plus.h"
+//#include "../Chip/Include/core_cm3.h"
+//#include "../Chip/Include/core_cm4.h"
+//#include "../Chip/Include/core_cm4_simd.h"
+//#include "../Chip/Include/core_cmFunc.h"
+//#include "../Chip/Include/core_cmInstr.h"
+//#include "../Chip/Include/core_sc000.h"
+//#include "../Chip/Include/core_sc300.h"
+
+#include "../Chip/Driver/Include/lpc177x_8x_pinsel.h"
+//#include "./Chip/Driver/Include/lpc17xx.h"
+//#include "./Chip/Driver/Include/lpc17xx_8x_libcfg.h" // load library configuration
+#include "../Chip/Driver/Include/lpc177x_8x_gpio.h"
+#include "../Chip/Driver/Include/lpc177x_8x_clkpwr.h"
+#include "../Chip/Driver/Include/lpc177x_8x_uart.h"
+// ************ BOARD SUPPORT PACKAGE HEADERS *************************************
+//I "./BSP/Include"`
+ // #pragma GCC system_header
+
+// ************ FRAMEWORK HEADERS *************************************************
+#include "bsp.h"
+#pragma GCC system_header
+#include "fnet.h"
+#include "fnet_config.h"
+#include "fnet_isr.h"		// prepojenie CPU IRQ a FNET aplikacie
+#include "lpc_debug.h"
+
+// export from main.c module
+extern void cpu_serial_init(long port_number, unsigned long baud_rate);
+extern void FNET_START(void);
+extern void HW_initialize_ETH(void);
+
+#endif /* GLOBAL_H_ */
